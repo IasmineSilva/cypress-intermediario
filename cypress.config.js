c9a4b764529e5ad1c43b86519f008bcb4a3aee0e
Iasmine <Iasmine948@gmail.com>
2024-07-29 20:48:49 -0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,19 @@ module.exports = defineConfig({
   video: false,
 })
 
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
+    experimentalRunAllSpecs: true,
+  },
+  fixturesFolder: false,
+  video: false,
+})
+
+
